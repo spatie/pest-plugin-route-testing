43bfc98905f59195a0a7b18971f4eee71e283202
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2024-05-15 16:43:17 +0200

    make phpstan not require stuff like final

--- rector.php
+++ rector.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
 use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
 use Rector\Set\ValueObject\LevelSetList;
