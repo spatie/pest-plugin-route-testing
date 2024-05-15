f7c3fecaab44285e9bd28bb8c76cb7c4c5087dc6
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2024-05-15 16:38:30 +0200

    make rector a bit more likeable to use

--- rector.php
+++ rector.php
@@ -4,24 +4,39 @@ declare(strict_types=1);
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
+use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
+use Rector\CodeQuality\Rector\FunctionLike\SimplifyUselessVariableRector;
+use Rector\DeadCode\Rector\Array_\RemoveDuplicatedArrayKeyRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPrivateMethodRector;
+use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
+use Rector\Php81\Rector\Array_\FirstClassCallableRector;
+use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
+use Rector\Strict\Rector\BooleanNot\BooleanInBooleanNotRuleFixerRector;
+use Rector\Strict\Rector\If_\BooleanInIfConditionRuleFixerRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
         __DIR__.'/src',
     ]);
 
-    $rectorConfig->rules([
-        InlineConstructorDefaultToPropertyRector::class,
-    ]);
-
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_81,
-        SetList::CODE_QUALITY,
         SetList::DEAD_CODE,
         SetList::EARLY_RETURN,
         SetList::TYPE_DECLARATION,
-        SetList::PRIVATIZATION,
+    ]);
+
+    $rectorConfig->skip([
+        ReadOnlyPropertyRector::class,
+        ClosureToArrowFunctionRector::class,
+        FirstClassCallableRector::class,
+        NullToStrictStringFuncCallArgRector::class,
+        BooleanInIfConditionRuleFixerRector::class,
+        BooleanInBooleanNotRuleFixerRector::class,
+        RemoveUnusedPrivateMethodRector::class,
+        SimplifyUselessVariableRector::class,
+        RemoveDuplicatedArrayKeyRector::class,
     ]);
 };
